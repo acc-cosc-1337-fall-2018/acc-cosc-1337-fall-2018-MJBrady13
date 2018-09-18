1a669b4ad6816a08e661e90946428b591005619b
5960607 <5960607@HLC2413-12>
2018-09-18 18:05:34 -0500

    Pass const parameter; read only

--- 01-basics/basics.h
+++ 01-basics/basics.h
@@ -1,6 +1,8 @@
 ﻿int sum_numbers(int num1, int num2);
 
-//write function prototype for multiply_numbers with two integer parameters
 int multiply_numbers(int num1, int num2);
 
 void pass_by_val_by_ref(int val, int& ref);
+
+void const_pass_by_val_by_ref(int val, const int & ref);
+
--- 01-basics/main.cpp
+++ 01-basics/main.cpp
@@ -5,6 +5,8 @@ using std::cout;
 
 int main()
 {
+	const int read_only = 10;
+	//read_only = 15;
 	cout << sum_numbers(5, 5);
 
 	return 0;
