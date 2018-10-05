64c087ec77022055a72630d8c69b98f11f78d97c
5960607 <5960607@HLC2413-12>
2018-10-04 19:09:33 -0500

    assignment 6 completed

--- assignments/05-classes/invoice_detail.cpp
+++ assignments/05-classes/invoice_detail.cpp
@@ -5,3 +5,14 @@ double InvoiceDetail::get_extended_cost()
 {
 	return cost*units;
 }
+
+std::istream & operator>>(std::istream& in, InvoiceDetail & d)
+{
+	std::cout << "Enter Cost: ";
+	in  >> d.cost;
+	std::cout << "Enter Number of Units: ";
+	in  >> d.units;
+
+	return in;
+
+}
